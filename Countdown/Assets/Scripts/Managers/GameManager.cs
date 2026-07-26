e023c92e9d4bd66b35495ee4d84c5cd12c24cdf7
Benedict Clayton <100662252+Benedict-Clayton@users.noreply.github.com>
2026-07-26 20:26:44 +1000

    Adjusts

--- Countdown/Assets/Scripts/Managers/GameManager.cs
+++ Countdown/Assets/Scripts/Managers/GameManager.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static DamageManager;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class GameManager : MonoBehaviour
 {
